c25937edc9a150603b0170fe18ce14e7790a9ba4
HarshithReddy <2311cs010281@mallareddyuniversity.ac.in>
2024-06-28 16:45:08 +0530

    feat: Add JavaScript code to display current day, time, and current time with AM/PM

--- NewJavascript.js
+++ NewJavascript.js
@@ -1,4 +1,21 @@
-const num1 = 5;
-const num2 = 10;
-const sum = num1 + num2;
-console.log("The sum is: " + sum);
+
+// 1. Write a JavaScript program to display the current day and time in the following format.
+// Sample Output : Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth();
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+console.log("Today is: ",day);
+console.log("current time is :",hours + " PM : " +minutes+" :"+seconds);
+
+// write a javascript program to get current time with AM/PM
+const dateq = new Date();
+const hoursq = dateq.getHours();
+const minutesq = dateq.getMinutes();
+const secondsq = dateq.getSeconds();
+const ampm = hoursq >=12 ?'PM':'AM';
+console.log("current time is :",hoursq +" "+ampm);
